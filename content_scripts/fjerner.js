0c20974f8b79c37c8a993fb7e642841cf66caa0d
AFS0018213 <AFS0018213@agderskolen.no>
2023-08-19 18:57:17 +0200

    kan nå klikkes flere ganger

--- content_scripts/fjerner.js
+++ content_scripts/fjerner.js
@@ -1,5 +1,8 @@
-let ikkeLeste = document.body.querySelectorAll("[aria-label='Ikke lest varsel']");
-for (let ikkeLest of ikkeLeste) {
-    ikkeLest.click();
+function fjernVarsler() {
+    let ikkeLeste = document.body.querySelectorAll("[aria-label='Ikke lest varsel']");
+    for (let ikkeLest of ikkeLeste) {
+        ikkeLest.click();
+    }
+    alert("Fjernet");
 }
-alert("Fjernet");
+fjernVarsler()
